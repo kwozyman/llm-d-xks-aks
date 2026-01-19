8f0086df4537c2dd2965be754bdaa2fea817a612
Costin Gamenț <costin.gament@gmail.com>
2026-01-19 18:21:47 +0100

    Fix NRI tuning

--- azure/06-nri.sh
+++ azure/06-nri.sh
@@ -7,8 +7,9 @@ echo "--- 11. Deploying NRI Plugin ---"
 kubectl get nodes -o custom-columns=":metadata.name" --no-headers -l agentpool=gpunp | while read -r node; do
     echo "$node"
     kubectl debug "node/${node}" -it --image=busybox -- sh -c "cat <<EOF > /host/etc/containerd/conf.d/98-nri.toml
-[\"plugins.io.containerd.nri.v1.nri\"]
+[plugins.\"io.containerd.nri.v1.nri\"]
   disable = false"
+    kubectl debug "node/${node}" -it --image=busybox -- chroot /host systemctl restart containerd
 done
 
 kubectl apply -k https://github.com/containerd/nri/contrib/kustomize/ulimit-adjuster
