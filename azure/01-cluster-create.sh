4222383dbeb24736a5207d94f9ff2c691ce1587d
Costin Gamenț <costin.gament@gmail.com>
2026-01-30 15:35:39 +0100

    further generalize variables

--- azure/01-cluster-create.sh
+++ azure/01-cluster-create.sh
@@ -5,19 +5,18 @@ echo "--- 1. Creating Resource Group ---"
 az group create --name "${RESOURCE_GROUP}" --location "${LOCATION}"
 
 echo "--- 2. Creating AKS Cluster (System Pool) ---"
-az aks create --resource-group "${RESOURCE_GROUP}" --name "${CLUSTER_NAME}" --node-count 1 --node-vm-size "Standard_D2s_v3" --ssh-key-value "${SSH_KEY_FILE}"
+az aks create --resource-group "${RESOURCE_GROUP}" --name "${CLUSTER_NAME}" --node-count 1 --node-vm-size "${CONTROL_SKU}" --ssh-key-value "${SSH_KEY_FILE}"
 
 echo "--- 3. Adding GPU Node Pool (with Taint) ---"
-# This is a critical step: --gpu-driver none lets the operator manage it.
 az aks nodepool add \
     --resource-group "${RESOURCE_GROUP}" \
     --cluster-name "${CLUSTER_NAME}" \
     --name "gpunp" \
-    --node-count 1 \
+    --node-count "${NODE_COUNT}" \
     --node-vm-size "${GPU_SKU}" \
     --gpu-driver none \
     --labels "sku=gpu"
 
-echo "--- 4. Getting Cluster Credentials ---"
+echo "--- 3. Getting Cluster Credentials ---"
 az aks get-credentials --resource-group "${RESOURCE_GROUP}" --name "${CLUSTER_NAME}" --overwrite-existing
 
